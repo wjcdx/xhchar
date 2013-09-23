6cfce582a21b730fb9bace87dbe1745e2e21e1ff
wjcdx <wjcdx@qq.com>
2013-09-23 22:30:04 +0800

    [canvas] display partials with random positions in the canvas

--- js/Displayer.js
+++ js/Displayer.js
@@ -13,8 +13,30 @@ Displayer.prototype.display = function()
 			imgLabels[i].src = imgs[i];
 		}
 
+		var ch = parseInt(this.canvas.style.height);
+		var cw = parseInt(this.canvas.style.width);
+
 		for (var i in imgLabels) {
-			this.canvas.appendChild(imgLabels[i]);
+			var img = imgLabels[i];
+
+			img.style.position = "absolute";
+
+			var ih = img.height;
+			var iw = img.width;
+
+			var top = Math.round(Math.random() * ch);
+			var lft = Math.round(Math.random() * cw);
+
+			if (top + ih > ch)
+					top = ch - ih;
+
+			if (lft + iw > cw)
+					lft = cw - iw;
+
+			img.style.top = top + "px";
+			img.style.left = lft + "px";
+
+			this.canvas.appendChild(img);
 		}
 }
 
